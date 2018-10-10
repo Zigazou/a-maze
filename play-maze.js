c0a6fb32803f28341d72cf758c0335ad3cfd1942
Frédéric BISSON <zigazou@free.fr>
2018-10-10 08:10:36 +0200

    Added JSDoc comments

--- play-maze.js
+++ play-maze.js
@@ -1,13 +1,24 @@
+/**
+ * @file play-maze.js
+ * @author Frédéric BISSON <zigazou@free.fr>
+ * @version 1.0
+ *
+ * Run a maze.
+ */
 (function(urlstring) {
+    // Looks for the first a-maze tag.
     const mazeApp = new MazeApp(document.querySelector("a-maze"))
 
+    // Default maze identifier.
     let mazeURL = "maze-01"
 
+    // Looks for the "maze" parameter in the URL.
     if(urlstring) {
         const url = new URL(urlstring)
         const param = url.searchParams.get("maze");
         if(param) mazeURL = "mazes/" + param + ".json"
     }
 
+    // Runs the maze and goes back to the index page when the game ends.
     mazeApp.run(mazeURL).then(() => window.location.href = "index.html")
 }) (window.location.href)
